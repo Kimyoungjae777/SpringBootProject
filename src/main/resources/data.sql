5983d3e89633d948787ef7f5d875ae4f0c6cc622
kimyoungjae777 <kimyoungjae123@naver.com>
2023-10-15 12:03:17 +0900

    first commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO member (id, name) VALUES (1, '이름1')
-INSERT INTO member (id, name) VALUES (2, '이름2')
-INSERT INTO member (id, name) VALUES (3, '이름3')
+INSERT INTO member (id, name) VALUES (1, '김영재')
+INSERT INTO member (id, name) VALUES (2, '이현숙')
+INSERT INTO member (id, name) VALUES (3, '김유진')
+INSERT INTO member (id, name) VALUES (4, '김운태')
